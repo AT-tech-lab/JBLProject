c00102466ed6834dd48d8d01075b0203c0355149
= <=>
2021-03-14 18:41:41 +0530

    trste

--- IDBI/src/test/java/Bank/IDBI/LoginTest.java
+++ IDBI/src/test/java/Bank/IDBI/LoginTest.java
@@ -8,7 +8,8 @@ public class LoginTest {
 		System.out.println("this is my second login");
 		System.out.println("this is my third login");
 		System.out.println("this is checkin done by master after branch");
-		System.out.println("this is creation of conflict at testers branch");
+		System.out.println("this is creation of conflict at testers branch"); 
+		System.out.println("this is not here");
 		System.out.println("this is second connflict at master");
 		System.out.println("this is second of conflict at testers branch");
 	}
